fd7cec9d5fb01faf591a919e337687dc02294c5f
Daulet <da3goo@gmail.com>
2025-01-28 23:51:04 +0500

    Bugs fix

--- Backend/MainDir/main.go
+++ Backend/MainDir/main.go
@@ -47,7 +47,7 @@ func main() {
 
 	headers := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
 	methods := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE"})
-	origins := handlers.AllowedOrigins([]string{"http://127.0.0.1:5500"})
+	origins := handlers.AllowedOrigins([]string{"*"})
 	credentials := handlers.AllowCredentials()
 
 	log.Println("Starting server on :8080")
